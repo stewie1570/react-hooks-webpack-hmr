e00f28730b6f36a64466ef0b32370d970c681080
Stewart Anderson <stewie1570@gmail.com>
2020-01-19 13:31:03 -0700

    useReducer -> useState (in callback form).

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,20 +1,21 @@
-import React, { useReducer } from "react";
-import { initialState, reducer } from "../reducers/todo.reducer.js";
+import React, { useState } from "react";
+import { initialState, add, edit, remove } from "../reducers/todo.reducer.js";
 import Todo from "./Todo.jsx";
 import AddTodo from "./AddTodo.jsx";
 
 const TodoApp = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, setState] = useState(initialState);
+
   return (<>
     <AddTodo
-      add={text => dispatch({type: "add", text: text})}
+      add={text => setState(add({ text }))}
     />
     {state.todos.map(t => (
       <Todo
         key={t.id}
         todo={t}
-        remove={() => dispatch({type: "remove", id: t.id})}
-        edit={text => dispatch({type: "edit", id: t.id, text: text})}
+        remove={() => setState(remove({ id: t.id }))}
+        edit={text => setState(edit({ id: t.id, text: text }))}
       />
     ))}
   </>);
--- src/reducers/todo.reducer.js
+++ src/reducers/todo.reducer.js
@@ -9,43 +9,36 @@ export const initialState = {
   }],
 };
 
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case "add":
-      {
-        const newCounter = state.counter + 1;
-        const newTodo = {
-          id: newCounter,
-          text: action.text,
-        };
-        return {
-          counter: newCounter,
-          todos: [...state.todos, newTodo],
-        };
-      }
-    case "edit":
-      {
-        const idx = state.todos.findIndex(t => t.id === action.id);
-        const todo = Object.assign({}, state.todos[idx]);
-        todo.text = action.text;
-        const todos = Object.assign([], state.todos);
-        todos.splice(idx, 1, todo);
-        return {
-          counter: state.counter,
-          todos: todos,
-        };
-      }
-    case "remove":
-      {
-        const idx = state.todos.findIndex(t => t.id === action.id);
-        const todos = Object.assign([], state.todos);
-        todos.splice(idx, 1);
-        return {
-          counter: state.counter,
-          todos: todos,
-        };
-      }
-    default:
-      return state;
-  }
+export const add = action => state => {
+  const newCounter = state.counter + 1;
+  const newTodo = {
+    id: newCounter,
+    text: action.text,
+  };
+  return {
+    counter: newCounter,
+    todos: [...state.todos, newTodo],
+  };
 };
+
+export const edit = action => state => {
+  const idx = state.todos.findIndex(t => t.id === action.id);
+  const todo = Object.assign({}, state.todos[idx]);
+  todo.text = action.text;
+  const todos = Object.assign([], state.todos);
+  todos.splice(idx, 1, todo);
+  return {
+    counter: state.counter,
+    todos: todos,
+  };
+}
+
+export const remove = action => state => {
+  const idx = state.todos.findIndex(t => t.id === action.id);
+  const todos = Object.assign([], state.todos);
+  todos.splice(idx, 1);
+  return {
+    counter: state.counter,
+    todos: todos,
+  };
+}
